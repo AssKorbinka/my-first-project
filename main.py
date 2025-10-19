9f9010c8daa26d09e74dd2243cc84662ca65aae0
AssKorbinka <gleb.chernikov.99@mail.ru>
2025-10-19 13:57:13 +0300

    Add calculate function and improve hello message

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def hello():
     print("Hello, Git!")
+    print("This is my first Git project!")
+
+def calculate(a, b):
+    return a + b
     
 if __name__ == "__main__":
     hello()
+    result = calculate(5, 3)
+    print(f"5 + 3 = {result}")
